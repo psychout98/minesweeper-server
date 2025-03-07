d714cc2e8611c017fb6a280e45af0977cb5c6bc5
Noah Heath <noahadamheath@gmail.com>
2025-03-07 01:25:23 -0500

    idk

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server({
+  ...server,
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('Hello world');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(3001, () => {
+  console.log('listening on *:3001');
 });
