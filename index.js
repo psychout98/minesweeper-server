b3e7e6ef970f744094eefdfac5d2966bd6614441
Noah Heath <noahadamheath@gmail.com>
2025-03-07 17:36:19 -0500

    update

--- index.js
+++ index.js
@@ -14,8 +14,28 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+app.get('/randomId', (req, res) => {
+
+});
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+
+  socket.on("subscribe", (topics) => {
+    socket.join(topics);
+    io.to(topics).except(socket.id).emit("userJoined");
+  });
+
+  socket.on("disconnecting", () => {
+    io.to([...socket.rooms]).emit("mouseLeave", socket.id);
+  });
+
+  socket.on("mouseMove", (mouseData, gameId) => {
+    io.to(gameId).except(socket.id).emit("mouseMove", { ...mouseData, socketId: socket.id });
+  });
+
+  socket.on("uploadBoard", (board, gameId) => {
+    io.to(gameId).except(socket.id).emit("receiveBoard", board);
+  });
 });
 
 server.listen(3001, () => {
