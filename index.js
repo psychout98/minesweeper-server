afb52ad98c56bc5d1555383053f4a09edcd344a6
Noah Heath <noahadamheath@gmail.com>
2025-03-07 11:02:58 -0500

    path and cors

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server({
-  ...server,
+const io = new Server(server, {
+  path: '/socket',
   cors: {
-    origin: "http://localhost:3000"
+    origin: "*"
   }
 });
 
